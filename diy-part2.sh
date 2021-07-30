81f6a6ce73e357517cf3912cc90267e9a31b2655
daniel <daniel@nextplus-inc.com>
2021-07-30 19:15:17 +0800

    x86

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,5 +11,5 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
-wget http://hk-d.npls.io/pkgs/.config_redmi -O .config
+# sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
+wget http://hk-d.npls.io/pkgs/.config_x86 -O .config
